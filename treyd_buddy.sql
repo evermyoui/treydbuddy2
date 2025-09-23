6169a8b656bfa196d423087ff7ff37af87123d18
evermyoui <sexbythewind@gmail.com>
2025-09-23 19:55:57 +0800

    add sql table

---
+++ treyd_buddy.sql
@@ -0,0 +1,170 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 23, 2025 at 01:54 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `treyd_buddy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance_table`
+--
+
+CREATE TABLE `attendance_table` (
+  `attendance_id` varchar(50) NOT NULL,
+  `reserve_id` varchar(50) DEFAULT NULL,
+  `user_id` varchar(50) DEFAULT NULL,
+  `event_id` varchar(50) DEFAULT NULL,
+  `time` datetime NOT NULL,
+  `attendance_status` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `billing_table`
+--
+
+CREATE TABLE `billing_table` (
+  `billing_id` varchar(50) NOT NULL,
+  `user_id` varchar(50) DEFAULT NULL,
+  `event_id` varchar(50) DEFAULT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `billing_date` date NOT NULL,
+  `payment_status` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_table`
+--
+
+CREATE TABLE `event_table` (
+  `event_id` varchar(50) NOT NULL,
+  `event_name` varchar(100) NOT NULL,
+  `event_date` date NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reserve_table`
+--
+
+CREATE TABLE `reserve_table` (
+  `reserve_id` varchar(50) NOT NULL,
+  `event_id` varchar(50) DEFAULT NULL,
+  `user_id` varchar(50) DEFAULT NULL,
+  `billing_id` varchar(50) DEFAULT NULL,
+  `reserve_date` date NOT NULL,
+  `status` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_table`
+--
+
+CREATE TABLE `user_table` (
+  `user_id` varchar(20) NOT NULL,
+  `last_name` varchar(20) NOT NULL,
+  `first_name` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendance_table`
+--
+ALTER TABLE `attendance_table`
+  ADD PRIMARY KEY (`attendance_id`),
+  ADD KEY `reserve_id` (`reserve_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `event_id` (`event_id`);
+
+--
+-- Indexes for table `billing_table`
+--
+ALTER TABLE `billing_table`
+  ADD PRIMARY KEY (`billing_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `event_id` (`event_id`);
+
+--
+-- Indexes for table `event_table`
+--
+ALTER TABLE `event_table`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- Indexes for table `reserve_table`
+--
+ALTER TABLE `reserve_table`
+  ADD PRIMARY KEY (`reserve_id`),
+  ADD KEY `event_id` (`event_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `billing_id` (`billing_id`);
+
+--
+-- Indexes for table `user_table`
+--
+ALTER TABLE `user_table`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendance_table`
+--
+ALTER TABLE `attendance_table`
+  ADD CONSTRAINT `attendance_table_ibfk_1` FOREIGN KEY (`reserve_id`) REFERENCES `reserve_table` (`reserve_id`),
+  ADD CONSTRAINT `attendance_table_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`),
+  ADD CONSTRAINT `attendance_table_ibfk_3` FOREIGN KEY (`event_id`) REFERENCES `event_table` (`event_id`);
+
+--
+-- Constraints for table `billing_table`
+--
+ALTER TABLE `billing_table`
+  ADD CONSTRAINT `billing_table_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`),
+  ADD CONSTRAINT `billing_table_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `event_table` (`event_id`);
+
+--
+-- Constraints for table `reserve_table`
+--
+ALTER TABLE `reserve_table`
+  ADD CONSTRAINT `reserve_table_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event_table` (`event_id`),
+  ADD CONSTRAINT `reserve_table_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user_table` (`user_id`),
+  ADD CONSTRAINT `reserve_table_ibfk_3` FOREIGN KEY (`billing_id`) REFERENCES `billing_table` (`billing_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
